5156bc8cf5e07a8bf6ccf16677baad3117039538
Alexander <avmolinapro@gmail.com>
2020-03-16 16:39:33 +0100

    separacion de la plantilla HTML en componentes de Angular

--- src/app/ingresos/ingreso.component.ts
+++ src/app/ingresos/ingreso.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-ingreso',
+  selector: 'app-ingresos',
   templateUrl: './ingreso.component.html',
   styleUrls: ['./ingreso.component.css']
 })
